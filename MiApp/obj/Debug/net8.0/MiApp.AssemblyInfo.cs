389f103f0334e8be5a5c991b67e83ea3392910a9
Carlos <carlos.a.quintero@avanade.com>
2026-01-30 10:55:18 +0100

    Modificacion program.cs

--- MiApp/obj/Debug/net8.0/MiApp.AssemblyInfo.cs
+++ MiApp/obj/Debug/net8.0/MiApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+431babf0e34b5c50fbe1168c0835120328f24860")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8e06642c0a7aab524a20426f5c0b34e0f22b641")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
